b6559f48611f032e3ae234c3dff5b1e6e41be465
Abdul Samad Yaqoob <154105818+samad3600@users.noreply.github.com>
2024-09-12 23:12:18 +0530

    Relational_Pps.py

---
+++ Relational_Pps.py
@@ -0,0 +1,21 @@
+# Define two variables
+x = 10
+y = 20
+
+# Equal to (==)
+print(f"{x} == {y} is {x == y}")
+
+# Not equal to (!=)
+print(f"{x} != {y} is {x != y}")
+
+# Greater than (>)
+print(f"{x} > {y} is {x > y}")
+
+# Less than (<)
+print(f"{x} < {y} is {x < y}")
+
+# Greater than or equal to (>=)
+print(f"{x} >= {y} is {x >= y}")
+
+# Less than or equal to (<=)
+print(f"{x} <= {y} is {x <= y}")
